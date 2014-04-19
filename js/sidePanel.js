befa964cdc74f2a594db87edd0daced3e951e787
tjb1015 <tjb1015@sru.edu>
2014-04-19 16:03:52 -0400

    sidePanel work

--- js/sidePanel.js
+++ js/sidePanel.js
@@ -12,31 +12,23 @@ var mMoveE = isTouchSupported ? 'touchmove' : 'mousemove';
 var mUpE = isTouchSupported ? 'touchend' : 'mouseup';
 
 
-function sidePanel() {
-
-
-
-
-
-}
-
 function panel() {
             //<rect fill="black" height="50"  width="25" stroke="red" stroke-width="2" x="20" y="20" ry="10" />
            // <rect fill="gray" height="34" width="2" stroke="gray" stroke-width="2" x="25" y="28" ry="1" />
            // <rect fill="gray" height="34" width="2" stroke="gray" stroke-width="2" x="30" y="28" ry="1" />
 
     //var sideClick = document.getElementById("sideClick")
-    this.g = document.createElementNS(xmlns, "g");
-    this.rect = document.createElementNS(xmlns,"rect");
-    this.rect.setAttributeNS(null, "height", "51");
-    this.rect.setAttributeNS(null, "width", "28");
-    this.rect.setAttributeNS(null, "fill", "black");
-    this.rect.setAttributeNS(null, "rx", "5");
-    this.rect.setAttributeNS(null, "stroke", "red");
-    this.rect.setAttributeNS(null, "stroke-width", "2");
-    this.rect.setAttributeNS(null, "x", (window.innerWidth - 26));
-    this.rect.setAttributeNS(null, "y", (window.innerHeight / 2 - 25));
-    this.g.appendChild(this.rect);
+    var g = document.createElementNS(xmlns, "g");
+    var rect = document.createElementNS(xmlns,"rect");
+    rect.setAttributeNS(null, "height", "51");
+    rect.setAttributeNS(null, "width", "28");
+    rect.setAttributeNS(null, "fill", "black");
+    rect.setAttributeNS(null, "rx", "5");
+    rect.setAttributeNS(null, "stroke", "red");
+    rect.setAttributeNS(null, "stroke-width", "2");
+    rect.setAttributeNS(null, "x", (window.innerWidth - 26));
+    rect.setAttributeNS(null, "y", (window.innerHeight / 2 - 25));
+    g.appendChild(rect);
     for (var i = 0; i < 3; i++) {
         var rect = document.createElementNS(xmlns, "rect");
         rect.setAttributeNS(null, "height", "34");
@@ -47,37 +39,63 @@ function panel() {
         rect.setAttributeNS(null, "stroke-width", "2");
         rect.setAttributeNS(null, "x", (window.innerWidth - 22 + (i * 7)));
         rect.setAttributeNS(null, "y", (window.innerHeight / 2 - 16));
-        this.g.appendChild(rect);
+        g.appendChild(rect);
     }
-    this.panel = document.createElementNS(xmlns, "rect");
-    this.panel.setAttributeNS(null, "height", window.innerHeight);
-    this.panel.setAttributeNS(null, "width", "2");
-    this.panel.setAttributeNS(null, "fill", "gray");
-    this.panel.setAttributeNS(null, "rx", "2");
-    this.panel.setAttributeNS(null, "stroke", "black");
-    this.panel.setAttributeNS(null, "stroke-width", "2");
-    this.panel.setAttributeNS(null, "x", (window.innerWidth - 2));
-    this.panel.setAttributeNS(null, "y", "0");
-    //this.sideClick.setAttributeNS(null, "translate", "transform(" + +"," + +")"
-    this.g.appendChild(this.panel);
-    root.appendChild(this.g);
+    var panel = document.createElementNS(xmlns, "rect");
+    panel.setAttributeNS(null, "height", window.innerHeight);
+    panel.setAttributeNS(null, "width", "2");
+    panel.setAttributeNS(null, "fill", "gray");
+    panel.setAttributeNS(null, "rx", "2");
+    panel.setAttributeNS(null, "stroke", "black");
+    panel.setAttributeNS(null, "stroke-width", "2");
+    panel.setAttributeNS(null, "x", (window.innerWidth - 2));
+    panel.setAttributeNS(null, "y", "0");
+    g.appendChild(panel);
+    root.appendChild(g);
+
+    g.open = false;
+
+    root.addEventListener(mUpE, function (evt) {
+        g.removeEventListener(mMoveE, movePanel, false);
+    }, false);
+
+    g.addEventListener(mDownE, function (evt) {
+        g.addEventListener(mMoveE, movePanel, false);
+    }, false);
+
+    g.addEventListener("click", function (evt) {
+        g.removeEventListener(mMoveE, movePanel, false);
+        g.open = true;
+        var sidePanel = evt.currentTarget;
+
+        sidePanel.childNodes[0].setAttributeNS(null, "x", window.innerWidth-350 - 26)
+        sidePanel.childNodes[1].setAttributeNS(null, "x", window.innerWidth-350 - 22)
+        sidePanel.childNodes[2].setAttributeNS(null, "x", window.innerWidth-350 - 15)
+        sidePanel.childNodes[3].setAttributeNS(null, "x", window.innerWidth-350 - 8)
+        sidePanel.childNodes[4].setAttributeNS(null, "x", window.innerWidth-350)
+        sidePanel.childNodes[4].setAttributeNS(null, "width", window.innerWidth-350)
+
+    }, false);
+
+};
+
+function movePanel(evt) {
+            mX = isTouchSupported ? evt.changedTouches[0].pageX : evt.clientX
+            mY = isTouchSupported ? evt.changedTouches[0].pageY : evt.clientY
+            var sidePanel = evt.currentTarget;
+
+            sidePanel.childNodes[0].setAttributeNS(null, "x", mX - 26 )
+            sidePanel.childNodes[1].setAttributeNS(null, "x", mX - 22)
+            sidePanel.childNodes[2].setAttributeNS(null, "x", mX - 15)
+            sidePanel.childNodes[3].setAttributeNS(null, "x", mX - 8)
+            sidePanel.childNodes[4].setAttributeNS(null, "x", mX)
+            sidePanel.childNodes[4].setAttributeNS(null, "width", window.innerWidth-mX)
+
+            //sidePanel.setAttributeNS(null, "x", (mx - 2));
+};
 
 
 
-    this.rect.addEventListener(mDownE, function (evt) {
-        mX = isTouchSupported ? evt.changedTouches[0].pageX : evt.clientX
-        mY = isTouchSupported ? evt.changedTouches[0].pageY : evt.clientY
 
 
-    });
-
-}
-function point(x, y) {
-    this.x = x;
-    this.y = y;
-}
-
-panel()
-
-
-
+panel();

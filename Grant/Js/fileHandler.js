ecd3bdf4121d62dfa826a6a5027e34ab2a28a502
zgp1001 <zgp1001@sru.edu>
2014-08-13 13:58:25 -0400

    Some changes to references

---
+++ Grant/Js/fileHandler.js
@@ -0,0 +1,43 @@
+function download(filename, data){
+	if(navigator.userAgent.indexOf(".NET") == -1) { //Real browsers 
+		var a = window.document.createElement('a');
+		a.href = window.URL.createObjectURL(new Blob([data], {type: 'text/plain;charset=utf-8;'}));
+		a.download = filename;
+
+		// Append anchor to body.
+		document.body.appendChild(a)
+		a.click();
+
+		// Remove anchor from body
+		document.body.removeChild(a)
+	} 
+	else {  //IE-Specific code, because...ya'know IE ... 
+		var content=data; 
+		var blob = new Blob([content],{
+		    type: "text/plain;charset=utf-8;"
+		});
+
+		navigator.msSaveBlob(blob, filename)
+	}
+}
+
+function readFile(ev){
+	var fr = new FileReader();
+	fr.onload = function(e) {
+		win.postMessage(fr.result, "*");
+		//loadXML(fr.result);  //This needs un-coupled 
+		//TODO: Make this work with Grapher (importClipboard())
+
+	}
+	fr.readAsText(ev.target.files[0]);
+}
+
+function loadFile(windowObject) {
+	win = windowObject 
+	var input = window.document.createElement('input');
+	input.type = 'file';
+	document.body.appendChild(input)
+	input.addEventListener('change', readFile);
+	input.click();
+	document.body.removeChild(input);
+}

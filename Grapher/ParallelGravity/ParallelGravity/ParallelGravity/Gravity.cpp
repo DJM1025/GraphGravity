6a7a8132dfe74fe2e0f0fdee470f2f22ce5b7b4c
nicholas-botzer <nicholas.botzer@gmail.com>
2015-02-03 11:29:05 -0500

    Parses xml for adjacency matrix
    The parallel gravity can parse the xml and get the adjacency matrix
    saved into the 2d array

--- Grapher/ParallelGravity/ParallelGravity/ParallelGravity/Gravity.cpp
+++ Grapher/ParallelGravity/ParallelGravity/ParallelGravity/Gravity.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <math.h>
 #include "pugixml-1.5\src\pugixml.hpp"
 
 using namespace std;
 using namespace pugi;
 
-
-int main(){
-
-	cout << "Hello world" << endl;
+int global_nodes;
+//parses the graph and creates the adjacency matrix. -- may have to add more to this later
+void parseGraph(int ***adjMatrix ) {
 	
+	int nodeCounter = 0; //counts the number of nodes in the graph
 	xml_document doc;
-
 	xml_parse_result result = doc.load_file("graph.xml");
-
 	xml_node nodes = doc.child("graph");
-
 	for(xml_node node = nodes.first_child(); node; node = node.next_sibling())
 	{
 
@@ -24,10 +23,66 @@ int main(){
 			cout << " " << attr.name() << "=" << attr.value();
 		}//end for attributes
 
+		nodeCounter++;
+		cout << endl;
+	}//end for
+	global_nodes = nodeCounter;
+	*adjMatrix = new int *[nodeCounter];
+	for(int x=0;x < nodeCounter;x++)
+	{
+		(*adjMatrix)[x] = new int[nodeCounter];
+	}//end for x
+
+	for(int r=0; r < nodeCounter;r++)
+	{
+		for(int c=0; c < nodeCounter;c++)
+		{
+			(*adjMatrix)[r][c] = 0;
+		}//end for c
+	}//end for r
+	int count = 0;
+
+
+	for(xml_node node = nodes.first_child(); node; node = node.next_sibling())
+	{
+		for(xml_node edge = node.child("edge"); edge; edge = edge.next_sibling("edge"))
+		{
+			string str = edge.attribute("to").as_string();
+			string stringTo = str.substr(1);
+			int edgeTo;
+
+			stringstream convert(stringTo);
+
+			if( !(convert >> edgeTo))
+				edgeTo = 0;			//this is the error case which sets the edgeTo to zero
+
+			(*adjMatrix)[count][edgeTo] = 1;
+			(*adjMatrix)[edgeTo][count] = 1;
+		}//end for each edge
+		count++;
 		cout << endl;
 	}//end for
 
-	//cout << "Load result: " << result.description() << ", node id: " << doc.child("node").attribute("id").value() << endl;
+}
+//function prints the adjacency matrix for the user to look at
+void printMatrix(int **ary)
+{
+	for(int r=0;r < global_nodes;r++)
+	{
+		for(int c=0;c < global_nodes;c++)
+		{
+			cout << "  " << ary[r][c] << "  ";
+		}
+		cout << endl;
+	}
+}
+int main(){
+	
+	int** adjMatrix; 
+
+	parseGraph(&adjMatrix);
+
+	printMatrix(adjMatrix);
 
 
 	cout << "-------Normal Termination---------------------" << endl;
